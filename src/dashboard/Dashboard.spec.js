e92a27a4be14af7033d065b4d405d19696cbd389
Emily_Richard <richard.emily.e@gmail.com>
2019-10-14 15:04:56 -0400

    Started tests on Display, render tests on all three files

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+test('Dashboard renders correctly.', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import { render, getByText } from '@testing-library/react';
+import Display from './Display';
+
+test('Display renders correctly.', () => {
+    expect(render(<Display />)).toMatchSnapshot();
+});
+
+test('Default setting is unlocked open.', () => {
+    const container = render(<Display />);
+    container.getByText(/unlocked/i);
+    container.getByText(/open/i);
+});
